b41dabee0772ee2dc000522810f9971fbd1ebee2
Carlos Beraun <cberaun2@cnm.edu>
2015-10-13 11:22:21 -0600

    Added gitignore for DDC project about-carlos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<html>
+		<tile>
+			About Carlos
+		</tile>
+	<body background="background.jpg">
+		<p>Abuot Carlos</p>
+		<br></br>
+		<p>I am Learning to Code</p>
+	</body>
+</html>
