a90c6c3da4261f9559789180eb6836c5ba859e71
Carlos Beraun <cberaun2@cnm.edu>
2015-10-13 13:32:57 -0600

    Added gitignore for DDC project about-carlos

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 			</head>
 
 		<header>
+				<div class="transbox"
 			<tile>
 			   <h1>Welcome to About Carlos</h1>
 		   </tile>
@@ -15,7 +16,7 @@
 		<div class="container"></div>
 			<p>Hi, My name is carlos Beraun i am from Huancayo Peru.</p>
 			<p>I am here to learn to code and to create software.</p>
-			<p>I help coach my sons soccer team and i also host some pretty awesome tea parties for my daughters, Invite only.</p>
+			<p>I help coach my sons soccer team and i also host some pretty awesome tea parties for my daughters, Invites only.</p>
 			<br></br>
 
 			<p>Early Years</p>
