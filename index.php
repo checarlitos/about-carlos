5abf1af084e9c09a4f277e8e3ab9a5d8082cc672
Carlos Beraun <cberaun2@cnm.edu>
2015-10-14 12:35:40 -0600

    Added gitignore for DDC project about-carlos

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 		</header>
 
 		<div class="container"></div>
-			<p>Hi, My name is carlos Beraun i am from Huancayo Peru.</p>
+			<h1>Hi, My name is carlos Beraun i am from Huancayo Peru.</h1>
 				<em>I am 32yr. I am married with some amazing children. I am a big fan of watching soccer form all parts of the world.</em>
 
 			<p>I help coach my sons soccer team and i also host some pretty awesome tea parties for my daughters, Invites only! sorry ladies.</p>
