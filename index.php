1cd9a00b5c5363ee6ceb74269dd9f426c2f05370
Carlos Beraun <cberaun2@cnm.edu>
2015-10-29 19:33:50 -0600

    Added text and buttons on the header

--- index.php
+++ index.php
@@ -26,8 +26,19 @@
 
 	<body>
 		<header>
-			<div class="container pull-left">
+			<div class="container pull-left btn-group">
 				<h1>Stemulus Mafia</h1>
+						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Action <span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+
 			</div>
 		</header>
 
