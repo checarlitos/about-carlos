6bb925a424e8af7e7c05542fac2501568f2098d7
Carlos Beraun <cberaun2@cnm.edu>
2015-10-31 18:44:35 -0600

    Added footer to bottom of page.

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
+	<!---------------------Header area--------------------------->
 	<head>
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-						<!-----Adding the Complied boot strap minifled css------->
-						<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<!-----Adding the Complied boot strap minifled css------->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-						<!-- Optional theme -->
-						<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+	<!-- Optional theme -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
 
-						<!-- Latest compiled and minified JavaScript -->
-						<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+	<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
-						<!--My custom stylesheet-------->
-						<link href="css/style.css" rel="stylesheet">
+	<!--My custom stylesheet-------->
+				<link href="css/style.css" rel="stylesheet">
 
 	</head>
 
-	<body class="sfooter">
-		<div class="sfooter-content">
+
 
 	<!-----Header text and navigation buttons-------->
+			<body class="sfooter">
+				<div class="sfooter-content">
+
+
 		<header>
 
 			<div class="container">
 						<div class="row col-md-12">
 
-						<h1>Stemulus Mafia</h1>
+							<h1>Stemulus Mafia</h1>
 
 								<div class="pull-right float">
 									<button type="button" class="btn btn-default">Left</button>
@@ -104,6 +109,8 @@
 
 
 </div>
+
+
 	</body>
 
 
