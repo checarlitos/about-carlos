704aa7dc71fe914c4cb7332dc6a4f967cdb3609b
Carlos Beraun <cberaun2@cnm.edu>
2015-10-31 18:58:49 -0600

    Added footer to bottom of page.

--- index.php
+++ index.php
@@ -103,13 +103,7 @@
 
 <!---------------Footer---------->
 							<div class="container">
-										<div class="page-header">
-											<h1>Sticky footer</h1>
-										</div>
-
-										<p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
-										<p>Use <a href="http://www.deepdivecoding.com">the sticky footer with a fixed navbar</a> if need be, too.</p>
-							</div>
+										<div class="page-header"
 
 							<footer class="footer">
 										<div class="container">
